b02504f08f6ceedc6428a3e054f29320f55f6d01
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-06-17 08:01:02 -0500

    firs-commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_store/app.dart';
+
+void main() {
+  runApp(const App());
+}
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,22 @@
+class TImages {
+  // App Logos
+  static const String darkAppLogo = "assets/logos/logo_dark.png";
+  static const String lightAppLogo = "assets/logos/logo_light.png";
+
+  // Social Logos
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+
+  // -- Onboarding Images
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/sammy-line-searching.gif";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/sammy-line-shopping.gif";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/sammy-line-delivery.gif";
+
+  // -- Animations
+  static const String productsIllustration = "assets/images/animations/sammy-line-workout.gif";
+  static const String productsSaleIllustration = "assets/images/animations/sammy-line-sale.gif";
+  static const String staticSuccessIllustration = "assets/images/animations/sammy-line-success.gif";
+  static const String deliveredInPlaneIllustration = "assets/images/animations/sammy-line-come-back-later.gif";
+  static const String deliveredEmailIllustration = "assets/images/animations/sammy-line-man-receives-a-mail.gif";
+  static const String verifyIllustration = "assets/images/animations/sammy-line-traver-backpack-with-passport-and-air-ticket.gif";
+}
