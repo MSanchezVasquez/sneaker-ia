9ed4578663145dcd076b65ab6568707fd2ea242b
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-06-22 23:47:29 -0500

    feat(home): integrate popular products grid into home screen" -m "- Update HomeScreen to display a grid of vertical product cards under the promo slider. - Utilize TGridLayout and TProductCardVertical for the popular products section.

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sneaker_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:sneaker_store/common/widgets/layouts/grid_layout.dart';
+import 'package:sneaker_store/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:sneaker_store/common/widgets/texts/section_heading.dart';
 import 'package:sneaker_store/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:sneaker_store/features/shop/screens/home/widgets/home_categories.dart';
@@ -53,10 +55,23 @@ class HomeScreen extends StatelessWidget {
 
             /// Body
             Padding(
-              padding: const EdgeInsets.all(TSizes.defaultSpace),
-              child: TPromoSlider(banners: [
-                TImages.promoBanner1, TImages.promoBanner2, TImages.promoBanner3
-              ],),
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// -- Promo Sliders
+                  const TPromoSlider(
+                    banners: [
+                      TImages.promoBanner1,
+                      TImages.promoBanner2,
+                      TImages.promoBanner3,
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  /// -- Popular Products
+                  TGridLayout(itemCount: 4, itemBuilder: (_, index) => const TProductCardVertical())
+                ],
+              ),
             ),
           ],
         ),
