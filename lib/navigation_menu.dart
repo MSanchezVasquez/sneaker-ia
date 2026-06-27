cb25754b3ccbc749604a93b5f3d4f9a5b6009b60
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-06-27 00:10:26 -0500

    feat(wishlist): create favourites screen and wire up navigation" -m "- Build FavouriteScreen utilizing TGridLayout and TProductCardVertical to display saved items. - Add an action button in the app bar to route back to HomeScreen. - Update NavigationController to replace the placeholder container with FavouriteScreen.

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:sneaker_store/features/shop/screens/home/home.dart';
 import 'package:sneaker_store/features/shop/screens/store/store.dart';
+import 'package:sneaker_store/features/shop/screens/wishlist/wishlist.dart';
 import 'package:sneaker_store/utils/constants/colors.dart';
 import 'package:sneaker_store/utils/helpers/helper_functions.dart';
 
@@ -43,7 +44,7 @@ class NavigationController extends GetxController {
   final screens = [
     const HomeScreen(),
     const StoreScreen(),
-    Container(color: Colors.orange),
+    const FavouriteScreen(),
     Container(color: Colors.blue),
   ];
 }
