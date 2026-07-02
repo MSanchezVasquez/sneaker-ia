416598037e4fa2edf50955abca0722d64d1a02ed
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-07-02 15:32:58 -0500

    feat(cart): build cart screen with item list and checkout action" -m "- Implement CartScreen displaying a ListView of TCartItem components. - Integrate the add/remove quantity buttons and item pricing. - Add a sticky bottom navigation bar with a 'Checkout' button.

---
+++ lib/features/shop/screens/cart/cart.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_store/common/widgets/appbar/appbar.dart';
+import 'package:sneaker_store/common/widgets/texts/product_price_text.dart';
+import 'package:sneaker_store/utils/constants/sizes.dart';
+import '../../../../common/widgets/products/cart/add_remove_button.dart';
+import '../../../../common/widgets/products/cart/cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        showBackArrow: true,
+        title: Text("Cart", style: Theme.of(context).textTheme.headlineSmall),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemCount: 4,
+          separatorBuilder: (_, __) =>
+              const SizedBox(height: TSizes.spaceBtwSections),
+          itemBuilder: (_, index) => Column(
+            children: [
+              const TCartItem(),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      /// Extra Space
+                      const SizedBox(width: 70),
+
+                      /// Add Remove Buttons
+                      TProductQuantityWithAddRemoveButton(),
+                    ],
+                  ),
+                  TProductPriceText(price: "256"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("Checkout \$256.0"),
+        ),
+      ),
+    );
+  }
+}
