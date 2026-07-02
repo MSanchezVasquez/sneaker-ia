fbe28871b2e2c71689dafc7665d62ef87ed5745b
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-07-02 15:32:47 -0500

    refactor(ui): enhance cart menu icon dynamically and add navigation" -m "- Update TCartCounterIcon to accept custom background/text colors for the badge badge, defaulting to Theme mode colors. - Wire up the icon's onPressed callback to navigate directly to CartScreen using Get.to().

--- lib/common/widgets/products/cart/cart_menu_icon.dart
+++ lib/common/widgets/products/cart/cart_menu_icon.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:sneaker_store/features/shop/screens/cart/cart.dart';
 import 'package:sneaker_store/utils/constants/colors.dart';
+import 'package:sneaker_store/utils/helpers/helper_functions.dart';
 
 class TCartCounterIcon extends StatelessWidget {
   const TCartCounterIcon({
-    super.key, this.iconColor, required this.onPressed,
+    super.key,
+    this.iconColor,
+    this.counterBgColor,
+    this.counterTextColor,
+    required this.onPressed,
   });
 
-  final Color? iconColor;
+  final Color? iconColor, counterBgColor, counterTextColor;
   final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Stack(
       children: [
         IconButton(
-          onPressed: onPressed,
+          onPressed: () => Get.to(() => const CartScreen()),
           icon: Icon(
             Iconsax.shopping_bag,
             color: iconColor,
@@ -27,7 +35,7 @@ class TCartCounterIcon extends StatelessWidget {
             width: 18,
             height: 18,
             decoration: BoxDecoration(
-              color: TColors.black.withValues(alpha: 0.5),
+              color: counterBgColor ?? (dark ? TColors.white : TColors.black),
               borderRadius: BorderRadius.circular(100),
             ),
             child: Center(
