6432a61ef4c4050557cbc28254589eb21a8f289f
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-07-09 18:57:23 -0500

    feat(core): initialize firebase application on startup" -m "- Generate firebase_options.dart with platform-specific Firebase routing keys. - Update main.dart to await Firebase.initializeApp() before runApp. - Update native C++ and Swift plugin registrants for macOS and Windows support.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sneaker_store/app.dart';
+import 'package:sneaker_store/firebase_options.dart';
 
-void main() {
+/// -- Entry point of Flutter App
+Future<void> main() async {
+
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Init Payment Methods
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+      (FirebaseApp value ) => Get.put(AuthenticationRepository())
+  );
+
+  // Todo: Initialize Authentication
+
+  // Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
