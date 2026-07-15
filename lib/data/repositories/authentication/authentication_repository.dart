e0daaeb0a257dbb777a00e254961b9088b054092
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-07-14 23:22:56 -0500

    feat(backend): implement auth and firestore user repositories" -m "- Update AuthenticationRepository with registerWithEmailAndPassword method. - Create UserRepository to handle saving UserModel records into the 'Users' Firestore collection.

---
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -0,0 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:sneaker_store/features/authentication/screens/login/login.dart';
+import 'package:sneaker_store/features/authentication/screens/onboarding/onboarding.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  /// Variables
+  final deviceStorage = GetStorage();
+  final _auth = FirebaseAuth.instance;
+
+  /// Called from main.dart on app launch
+  @override
+  void onReady() {
+    FlutterNativeSplash.remove();
+    screenRedirect();
+  }
+
+  /// Function to Show Relevant Screen
+  screenRedirect() async {
+    // Local Storage
+    if (kDebugMode) {
+      print("============== GET STORAGE Auth Repo ===============");
+      print(deviceStorage.read("IsFirstTime"));
+    }
+    deviceStorage.writeIfNull("IsFirstTime", true);
+    deviceStorage.read("IsFirstTime") != true
+        ? Get.offAll(() => const LoginScreen())
+        : Get.offAll(const OnBoardingScreen());
+  }
+
+  /* ------------------ Email & Password sign-in ------------------*/
+
+  /// [EmailAuthentication] - SignIn
+
+  /// [EmailAuthentication] - Register
+  Future<UserCredential> registerWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (_) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+}
