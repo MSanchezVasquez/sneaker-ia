e602ab9864e14192831abe3ce375af55ea63c775
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-07-15 22:22:03 -0500

    feat(auth): enhance auth repository with email verification routing and logout" -m "- Update screenRedirect() to evaluate user.emailVerified and route to NavigationMenu or VerifyEmailScreen accordingly. - Add sendEmailVerification() and logout() methods to communicate securely with FirebaseAuth.

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:sneaker_store/features/authentication/screens/login/login.dart';
 import 'package:sneaker_store/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:sneaker_store/features/authentication/screens/signup/verify_email.dart';
+import 'package:sneaker_store/navigation_menu.dart';
+
+import '../../../utils/exceptions/firebase_auth_exceptions.dart';
+import '../../../utils/exceptions/firebase_exceptions.dart';
+import '../../../utils/exceptions/format_exceptions.dart';
+import '../../../utils/exceptions/platform_exceptions.dart';
 
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
@@ -15,24 +20,35 @@ class AuthenticationRepository extends GetxController {
   final deviceStorage = GetStorage();
   final _auth = FirebaseAuth.instance;
 
-  /// Called from main.dart on app launch
   @override
   void onReady() {
+    // Remove the native splash screen
     FlutterNativeSplash.remove();
+    // Redirect to the appropiate screen
     screenRedirect();
   }
 
   /// Function to Show Relevant Screen
   screenRedirect() async {
-    // Local Storage
-    if (kDebugMode) {
-      print("============== GET STORAGE Auth Repo ===============");
-      print(deviceStorage.read("IsFirstTime"));
+    final user = _auth.currentUser;
+    if (user != null) {
+      if (user.emailVerified) {
+        Get.offAll(() => const NavigationMenu());
+      } else {
+        Get.offAll(() => VerifyEmailScreen(email: _auth.currentUser?.email));
+      }
+    } else {
+      // Local Storage
+      deviceStorage.writeIfNull("IsFirstTime", true);
+      // Check if it's the first time launching the app
+      deviceStorage.read("IsFirstTime") != true
+          ? Get.offAll(
+              () => const LoginScreen(),
+            ) // Redirect to Login Screen if not the first time
+          : Get.offAll(
+              const OnBoardingScreen(),
+            ); // Redirect to OnBoarding Screen if it's the first time
     }
-    deviceStorage.writeIfNull("IsFirstTime", true);
-    deviceStorage.read("IsFirstTime") != true
-        ? Get.offAll(() => const LoginScreen())
-        : Get.offAll(const OnBoardingScreen());
   }
 
   /* ------------------ Email & Password sign-in ------------------*/
@@ -58,6 +74,43 @@ class AuthenticationRepository extends GetxController {
     } on PlatformException catch (e) {
       throw TPlatformException(e.code).message;
     } catch (_) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+
+  /// [EmailVerification] - MAIL VERIFICATION
+  Future<void> sendEmailVerification() async {
+    try {
+      await _auth.currentUser?.sendEmailVerification();
+    } on FirebaseAuthException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw "Something went wrong. Please try again";
+    }
+  }
+
+  /// [ReAuthenticate] - ReAuthenticate User
+
+  /// [LogoutUser] - Valid for any authentication
+  Future<void> logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      Get.offAll(() => const LoginScreen());
+    } on FirebaseAuthException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
       throw 'Something went wrong. Please try again';
     }
   }
