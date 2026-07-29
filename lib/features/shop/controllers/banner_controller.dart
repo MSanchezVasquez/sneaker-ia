d8b979f4761bad2a917e04365959d8929742205c
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-07-29 17:49:25 -0500

    feat(home): implement banner and home state controllers" -m "- Create BannerController to fetch active banners and manage carousel dot indicators via Obx. - Create initial HomeController layout for future home-specific state management.

---
+++ lib/features/shop/controllers/banner_controller.dart
@@ -0,0 +1,46 @@
+import "package:get/get.dart";
+import "package:sneaker_store/data/repositories/banners/banner_repository.dart";
+
+import "package:sneaker_store/features/shop/models/banner_model.dart";
+
+import "../../../utils/popups/loaders.dart";
+
+class BannerController extends GetxController{
+
+  /// Variables
+  final isLoading = false.obs;
+  final carousalCurrentIndex = 0.obs;
+  final RxList<BannerModel> banners = <BannerModel>[].obs;
+
+  @override
+  void onInit() {
+    fetchBanners();
+    super.onInit();
+  }
+
+  /// Update Page Navigational Dots
+  void updatePageIndicator(index) {
+    carousalCurrentIndex.value = index;
+  }
+
+  /// Fetch Banners
+  Future<void> fetchBanners() async {
+    try {
+      // Show loader while loading banners
+      isLoading.value = true;
+
+      // Fetch Banners
+      final bannerRepo = Get.put(BannerRepository());
+      final banners = await bannerRepo.fetchBanners();
+
+      // Asign Banners
+      this.banners.assignAll(banners);
+
+    } catch(e) {
+      TLoaders.errorSnackBar(title: "Oh Snap!", message: e.toString());
+    } finally {
+      // Remove Loader
+      isLoading.value = false;
+    }
+  }
+}
